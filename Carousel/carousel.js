049e1597ab402aa3cd82294baeb8c9c698390825
saymon85 <saymon_bg@hotmail.com>
2019-12-16 15:58:24 +0100

    add function for initialize and move carousel

--- Carousel/carousel.js
+++ Carousel/carousel.js
@@ -1,3 +1,4 @@
+const itemClassName = 'carousel_photo';
 const items = document.querySelectorAll('.carousel_photo');
 const totalItems = items.length;
 let slide = 0;
@@ -5,7 +6,8 @@ let moving = true;
 
 //set initial classes
 
-const setInitialClasses = () => {
+function setInitialClasses(){
+
     items[totalItems - 1].classList.add('prev');
     items[0].classList.add('active');
     items[1].classList.add('next');
@@ -13,9 +15,10 @@ const setInitialClasses = () => {
 
 // add event listeners
 
-const setEventListeners = () => {
-    const prev = document.querySelector('carousel_button_prev');
-    const next = document.querySelector('carousel_button_next');
+function setEventListeners() {
+
+    const prev = document.querySelector('.carousel_button_prev');
+    const next = document.querySelector('.carousel_button_next');
 
     prev.addEventListener('click', moveNext);
     next.addEventListener('click', movePrev);
@@ -24,7 +27,7 @@ const setEventListeners = () => {
 //previous and next navigation handler functions
 
 
-const moveNext = () => {
+function moveNext(){
 
     if(!moving){
         if(slide === (totalItems - 1)){
@@ -36,7 +39,7 @@ const moveNext = () => {
     moveCarousel(slide);
 }
 
-const movePrev = () => {
+function movePrev(){
     if(!moving){
         if(slide === 0){
             slide = totalItems - 1;
@@ -44,12 +47,12 @@ const movePrev = () => {
             slide--;
         }
     }
-    moveCarousel(sldie);
+    moveCarousel(slide);
 }
 
 // disable intercation when slides rotate
 
-const disableInteraction = () => {
+function disableInteraction() {
 
     moving = true;
 
@@ -58,3 +61,50 @@ const disableInteraction = () => {
     }, 500);
 }
 
+// carousel moving function
+
+function moveCarousel(slide){
+    
+    if(!moving){
+        disableInteraction();
+    }
+    let newPrevious = slide - 1;
+    let newNext = slide + 1;
+    let oldPrevious = slide - 2;
+    let oldNext = slide + 2;
+
+    if((totalItems - 1) > 3) {
+        if(newPrevious <= 0){
+            oldPrevious = totalItems - 1;
+        }else if(newNext >= (totalItems - 1)){
+            oldNext = 0;
+        }
+        if(slide === 0){
+            newPrevious = totalItems - 1;
+            oldPrevious = totalItems - 2;
+            oldNext = slide + 1;
+        }else if(slide === (totalItems - 1)){
+            newPrevious = slide - 1;
+            newNext = 0;
+            oldNext = 1;
+        }
+
+        items[oldPrevious].className = itemClassName;
+        items[oldNext].className = itemClassName;
+        
+        items[newPrevious].className = itemClassName + ' prev';
+        items[slide].className = itemClassName + ' active';
+        items[newNext].className = itemClassName + ' next';
+    }
+
+}
+
+function initCarousel(){
+    
+        setInitialClasses();
+        setEventListeners();
+
+        moving = false;
+}
+
+initCarousel();
